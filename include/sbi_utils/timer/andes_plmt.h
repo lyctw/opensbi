630e6005fb6ecaa42547fabd011935fe44e9cc9e
Yu Chien Peter Lin <peterlin@andestech.com>
2022-08-22 16:49:15 +0800

    lib: utils/timer: Add Andes fdt timer support
    Since we can get the PLMT base address and timer frequency from
    device tree, move plmt timer device to fdt timer framework and
    also make it configurable using kconfig.
    
    This driver can be used with 25, 27 and 45 series CPUs.
    dts example (ax45mp 4 core):
    
      cpus {
          ...
          timebase-frequency = <0x3938700>;
          ...
      }
      soc {
          ...
          plmt0@e6000000 {
              compatible = "riscv,plmt0";
              reg = <0x00 0xe6000000 0x00 0x100000>;
              interrupts-extended = <&cpu0_intc 0x07
                                     &cpu1_intc 0x07
                                     &cpu2_intc 0x07
                                     &cpu3_intc 0x07>;
          };
          ...
      }
    
    Signed-off-by: Yu Chien Peter Lin <peterlin@andestech.com>

---
+++ include/sbi_utils/timer/andes_plmt.h
@@ -0,0 +1,32 @@
+/*
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright (c) 2022 Andes Technology Corporation
+ *
+ * Authors:
+ *   Zong Li <zong@andestech.com>
+ *   Nylon Chen <nylon7@andestech.com>
+ *   Yu Chien Peter Lin <peterlin@andestech.com>
+ */
+
+#include <sbi/riscv_asm.h>
+#include <sbi/riscv_io.h>
+#include <sbi/sbi_timer.h>
+
+#ifndef __TIMER_ANDES_PLMT_H__
+#define __TIMER_ANDES_PLMT_H__
+
+#define DEFAULT_AE350_PLMT_FREQ 60000000
+
+struct plmt_data {
+  u32 hart_count;
+  unsigned long size;
+  volatile u64 *time_val;
+  volatile u64 *time_cmp;
+};
+
+u64 plmt_timer_value(void);
+void plmt_timer_event_stop(void);
+void plmt_timer_event_start(u64 next_event);
+
+#endif /* __TIMER_ANDES_PLMT_H__ */
