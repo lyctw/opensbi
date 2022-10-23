9f5e9296575d9a5d5da9c6df50a224f4eb7602e5
Yu Chien Peter Lin <peterlin@andestech.com>
2022-10-23 20:21:09 +0800

    platform: rzf-dev: Update objects.mk
    Signed-off-by: Yu Chien Peter Lin <peterlin@andestech.com>

--- platform/renesas/rzf-dev/objects.mk
+++ platform/renesas/rzf-dev/objects.mk
@@ -1,12 +1,35 @@
 #
 # SPDX-License-Identifier: BSD-2-Clause
 #
-# Copyright (c) 2019 Andes Technology Corporation
-#
-# Authors:
-#   Zong Li <zong@andestech.com>
-#   Nylon Chen <nylon7@andestech.com>
-#
-# Copyright (c) 2021 Renesas Electronics Corporation
+# Copyright (c) 2022 Renesas Electronics Corporation
 
-platform-objs-y += cache.o platform.o plicsw.o plmt.o pma.o
+# Compiler flags
+platform-cppflags-y =
+platform-cflags-y =
+platform-asflags-y =
+platform-ldflags-y =
+
+# Objects to build
+platform-objs-y += cache.o platform.o pma.o
+
+# Blobs to build
+FW_TEXT_START=0x44000000
+
+FW_DYNAMIC=y
+
+FW_JUMP=n
+ifeq ($(PLATFORM_RISCV_XLEN), 32)
+  FW_JUMP_ADDR=0x400000
+else
+  FW_JUMP_ADDR=0x200000
+endif
+FW_JUMP_FDT_ADDR=0x2000000
+
+FW_PAYLOAD=n
+ifeq ($(PLATFORM_RISCV_XLEN), 32)
+  FW_PAYLOAD_OFFSET=0x400000
+else
+  FW_PAYLOAD_OFFSET=0x200000
+endif
+
+FW_PAYLOAD_FDT_ADDR=0x2000000
