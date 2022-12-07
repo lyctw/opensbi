f0e3dd0e6e93fbdb09ae542ffb0d6594b36ade2f
Yu Chien Peter Lin <peterlin@andestech.com>
2022-12-07 09:22:58 +0800

    add micro id field mask of marchid
    Signed-off-by: Yu Chien Peter Lin <peterlin@andestech.com>

--- platform/generic/include/andes/andes45.h
+++ platform/generic/include/andes/andes45.h
@@ -1,6 +1,8 @@
 #ifndef _RISCV_ANDES45_H
 #define _RISCV_ANDES45_H
 
+#define CSR_MARCHID_MICROID	0xfff
+
 /* Memory and Miscellaneous Registers */
 #define CSR_MCACHECTL		0x7ca
 #define CSR_MCCTLCOMMAND	0x7cc
