3cef16c5b8b16dcd1b3b2b512dcb09d85e484870
Yu Chien Peter Lin <peterlin@andestech.com>
2022-12-12 19:07:12 +0800

    sbi_types.h: add naked attribute
    Signed-off-by: Yu Chien Peter Lin <peterlin@andestech.com>

--- include/sbi/sbi_types.h
+++ include/sbi/sbi_types.h
@@ -63,6 +63,7 @@ typedef unsigned long		physical_size_t;
 
 #define __packed		__attribute__((packed))
 #define __noreturn		__attribute__((noreturn))
+#define __naked		__attribute__((naked))
 #define __always_inline	inline __attribute__((always_inline))
 #define __aligned(x)		__attribute__((aligned(x)))
 
