981547a043a6c0958a305ff68c853c3c100140b1
Lad Prabhakar <prabhakar.mahadev-lad.rj@bp.renesas.com>
2023-01-11 10:15:11 +0000

    platform: generic: renesas: rzfive: Configure the Local memory regions as part of root domain
    Renesas RZ/Five RISC-V SoC has Instruction local memory and Data local
    memory (ILM & DLM) mapped between region 0x30000 - 0x4FFFF. When a
    virtual address falls within this range, the MMU doesn't trigger a page
    fault; it assumes the virtual address is a physical address which can
    cause undesired behaviours for statically linked applications/libraries.
    
    To avoid this, add the ILM/DLM memory regions to the root domain region
    of the PMPU with permissions set to 0x0 so that any access to these
    regions gets blocked.
    
    Signed-off-by: Lad Prabhakar <prabhakar.mahadev-lad.rj@bp.renesas.com>

--- platform/generic/renesas/rzfive/rzfive.c
+++ platform/generic/renesas/rzfive/rzfive.c
@@ -5,7 +5,24 @@
  */
 
 #include <platform_override.h>
+#include <sbi/sbi_domain.h>
 #include <sbi_utils/fdt/fdt_helper.h>
+
+int renesas_rzfive_early_init(bool cold_boot, const struct fdt_match *match)
+{
+	/*
+	 * Renesas RZ/Five RISC-V SoC has Instruction local memory and
+	 * Data local memory (ILM & DLM) mapped between region 0x30000
+	 * to 0x4FFFF. When a virtual address falls within this range,
+	 * the MMU doesn't trigger a page fault; it assumes the virtual
+	 * address is a physical address which can cause undesired
+	 * behaviours for statically linked applications/libraries. To
+	 * avoid this, add the ILM/DLM memory regions to the root domain
+	 * region of the PMPU with permissions set to 0x0 so that any
+	 * access to these regions gets blocked.
+	 */
+	return sbi_domain_root_add_memrange(0x30000, 0x20000, 0x1000, 0x0);
+}
 
 static const struct fdt_match renesas_rzfive_match[] = {
 	{ .compatible = "renesas,r9a07g043f01" },
@@ -14,4 +31,5 @@ static const struct fdt_match renesas_rzfive_match[] = {
 
 const struct platform_override renesas_rzfive = {
 	.match_table = renesas_rzfive_match,
+	.early_init = renesas_rzfive_early_init,
 };
