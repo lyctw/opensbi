432a7763a378abbbc8181cd854c1496a7405e86b
Yu Chien Peter Lin <peterlin@andestech.com>
2023-01-18 14:50:19 +0800

    lib/utils/sys/atcsmu.c
    Signed-off-by: Yu Chien Peter Lin <peterlin@andestech.com>

--- lib/utils/sys/atcsmu.c
+++ lib/utils/sys/atcsmu.c
@@ -11,18 +11,26 @@
 #include <sbi/sbi_error.h>
 #include <andes/andes45.h>
 
-extern struct smu_data smu;
-
-void smu_set_wakeup_events(u32 events, u32 hartid)
+inline int smu_set_wakeup_events(struct smu_data *smu, u32 events, u32 hartid)
 {
-	writel(events, (void *)(smu.addr + PCSm_WE_OFFSET(hartid)));
+	if (smu) {
+		writel(events, (void *)(smu->addr + PCSm_WE_OFFSET(hartid)));
+		return 0;
+	}
+	else
+		return SBI_EINVAL;
 }
 
-bool smu_support_sleep_mode(u32 sleep_mode, u32 hartid)
+inline bool smu_support_sleep_mode(struct smu_data *smu, u32 sleep_mode, u32 hartid)
 {
 	u32 pcs_cfg;
 
-	pcs_cfg = readl((void *)(smu.addr + PCSm_CFG_OFFSET(hartid)));
+	if (!smu) {
+		sbi_printf("%s(): Fail to access smu_data\n", __func__);
+		return false;
+	}
+
+	pcs_cfg = readl((void *)(smu->addr + PCSm_CFG_OFFSET(hartid)));
 
 	switch (sleep_mode) {
 	case LIGHTSLEEP_MODE:
@@ -46,15 +54,37 @@ bool smu_support_sleep_mode(u32 sleep_mode, u32 hartid)
 	return true;
 }
 
-void smu_set_command(u32 pcs_ctl, u32 hartid)
+inline int smu_set_command(struct smu_data *smu, u32 pcs_ctl, u32 hartid)
 {
-	writel(pcs_ctl, (void *)(smu.addr + PCSm_CTL_OFFSET(hartid)));
+	if (smu) {
+		writel(pcs_ctl, (void *)(smu->addr + PCSm_CTL_OFFSET(hartid)));
+		return 0;
+	}
+	else
+		return SBI_EINVAL;
 }
 
-void smu_set_reset_vector(ulong wakeup_addr, u32 hartid)
+inline int smu_set_reset_vector(struct smu_data *smu, ulong wakeup_addr, u32 hartid)
 {
+	u32 vec_lo, vec_hi;
+	u64 reset_vector;
+
+	if (!smu)
+		return SBI_EINVAL;
+
 	writel(wakeup_addr,
-	       (void *)(smu.addr + HARTn_RESET_VEC_LO(hartid)));
+	       (void *)(smu->addr + HARTn_RESET_VEC_LO(hartid)));
 	writel((u64)wakeup_addr >> 32,
-	       (void *)(smu.addr + HARTn_RESET_VEC_HI(hartid)));
+	       (void *)(smu->addr + HARTn_RESET_VEC_HI(hartid)));
+
+	vec_lo = readl((void *)(smu->addr + HARTn_RESET_VEC_LO(hartid)));
+	vec_hi = readl((void *)(smu->addr + HARTn_RESET_VEC_HI(hartid)));
+	reset_vector = ((u64)vec_hi << 32) | vec_lo;
+
+	if(reset_vector != (u64)wakeup_addr) {
+		sbi_printf("hard%d (PCS%d): Failed to program the reset vector.\n",
+				hartid, hartid + 3);
+		return SBI_EFAIL;
+	} else
+		return 0;
 }
